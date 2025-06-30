3a2d328785e9876af6797d8bb2968053b2edd21c
Bert9597 <jasonruth4@gmail.com>
2025-06-30 21:19:32 +0200

    Create summary.py

---
+++ summary.py
@@ -0,0 +1,32 @@
+import json
+from datetime import datetime, timedelta
+from telegram import Bot
+import os
+
+now = datetime.now()
+past_week = now-timedelta(days=7)
+buyorders = os.getenv("FILE_PATH_BUYORDERS")
+api_keys = json.loads(os.getenv('API_KEYS'))
+weekly_profit = []
+total_profit = []
+token = api_keys['token']
+chat_id = api_keys["chat_id"]
+bot = Bot(token=token)
+
+with open(buyorders, "r") as f:
+    data = json.load(f)
+    for order in data:
+        if "sold" in data:
+            if order["date"] >= past_week:
+                weekly_profit.append(order['eur_profit'])
+            total_profit.append(order['eur_profit']) 
+        
+if total_profit:
+    bot.send_message(f"Totaal gerealiseerde inkomsten: €{round(sum(total_profit),2)}")
+    
+if weekly_profit:
+    total_weekly_profit = round(sum(weekly_profit),2)
+    avg_weekly_profit_per_trade = round(total_weekly_profit / len(weekly_profit),2)
+    bot.send_message(f"Weekoverzicht\n"
+                     f"Inkomsten: €{total_weekly_profit}\n"
+                     f"Gemmidelde winst per trade: €{avg_weekly_profit_per_trade}")
