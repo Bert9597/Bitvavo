5ee8e3a4d83f6c8f773f34f19ce0e614fbc5e6d9
Bert9597 <jasonruth4@gmail.com>
2025-07-02 14:35:52 +0200

    Update BOT.py

--- BOT.py
+++ BOT.py
@@ -349,6 +349,9 @@ class apibot():
                     data = json.load(f)
                     for order in data:
                         for i in open_orders:
+                            if order['Id'] != i['orderId']:
+                                order['type'] = "Sold"
+                                
                             if order['market'] == market and order['type'] == 'Open':
                                 profit = round((float(current_price) - float(order['price'])) / float(order['price']) * 100, 2)
                                 order['huidige_marktprijs'] = current_price 
